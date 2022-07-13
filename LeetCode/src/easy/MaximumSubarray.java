0e4c734d237fd7697c0a70f6365e21f9d8a9cef9
Jack <j.vandyket@gmail.com>
2022-07-12 20:49:23 -0400

    Add files via upload

---
+++ LeetCode/src/easy/MaximumSubarray.java
@@ -0,0 +1,20 @@
+package easy;
+
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        if(nums.length == 1) return nums[0];
+
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+
+        for(int n : nums){
+            sum += n;
+            max = Math.max(max, sum);
+
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
